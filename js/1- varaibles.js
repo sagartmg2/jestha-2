c53719b23c97a94afa74f7e275e60772a69ce31d
Sagar <dev.sagartmg@gmail.com>
2023-07-12 16:39:39 +0545

    variables

---
+++ js/1- varaibles.js
@@ -0,0 +1,105 @@
+console.log("Hello World")
+// Console.Log( '!! Hello World !! ');  // programming is case Sensitive
+console.log("Mern Classs")
+
+/* comments
+     ctrl + /  -- line comment
+     shift + alt + a  -- block comment 
+*/
+
+/* 
+    to print in different languages
+        python print("Hello world ")
+        php  echo("Hello World");  // semi colon is compulsary 
+        java System.out.println("Hello world");  //semi colon compulsary.
+
+*/
+
+
+console.log(100)
+console.log(100.30)
+console.log(true)
+console.log(false)
+
+
+/* Data Types
+    String -- text
+    Number 
+        - integer -- whole number
+        - float  -- decimal 
+    Boolean 
+        - true
+        - false
+*/
+
+
+/*  css-variable --primary-color:red */
+
+/* VARIABLES // re-usability 
+
+  <Keyword> = <value>  // value should be of certain data-type
+  eg.
+    name = "ram"
+    age = 10
+
+*/
+
+course = "mern"
+console.log("course:", course)
+age = 10
+console.log("age", age)
+color = "white"
+console.log("color:", color)
+
+is_married = true  // snake_case
+isMarried = false  // camelCase
+
+// full_name = "ram sharma"
+// fullName = 'ram sharma'
+
+// 1number = 100   // invalid
+// $number = 100
+number1 = 100
+
+
+
+firstName = "Ram"
+middleName = "Bdr"
+lastName = "sharma"
+
+fullName = firstName + " " + middleName + " " + lastName
+console.log("full name = ", fullName)
+
+
+
+
+/* var vs let vs const */
+
+var brand = "apple" // donot use var
+let price = 100
+const PI = 3.14
+
+
+/* DECLARATION AND INITIALZIATION */
+let size = "M"  // size varaible declared and m value assigned/initialized
+
+
+let count // declartion
+count = 100 // initialization
+count = 200 // re-initialization
+console.log("size", size)
+console.log("count", count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
