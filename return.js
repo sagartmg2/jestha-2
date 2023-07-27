14530b12d9b4448575cc923a9a995389af957e6e
Sagar <dev.sagartmg@gmail.com>
2023-07-27 16:46:45 +0545

    forEach and return

---
+++ return.js
@@ -0,0 +1,53 @@
+/* 
+function sum(num1, num2) {
+    return num1 + num2
+} 
+*/
+
+
+// const sum = (num1, num2) => {
+//     return num1 + num2
+// }
+
+const sum = (num1, num2) => num1 + num2
+
+
+console.log("sum is ", sum(2, 4))
+
+
+
+console.log("type", typeof (1212))
+
+
+// const createPerson = (name,age) => {
+//     return { name,age}
+// }
+
+const createPerson = (name, age) => ({ name, age })
+
+
+console.log(createPerson("ram", 20))
+
+
+// const double = (num) => num * 2
+const double = num => num * 2
+
+console.log(double(2))
+console.log(double(3))
+
+
+let fruites = ["apple", "orange", "kiwi"]
+
+// fruites[0]
+// fruites[1]
+// fruites[2]
+
+// for (let index = 0; index < fruites.length; index++) {
+//     console.log(fruites[index])
+// }
+
+fruites.forEach((element,index) => {
+    console.log(element)
+})
+
+/* map find filter  */
